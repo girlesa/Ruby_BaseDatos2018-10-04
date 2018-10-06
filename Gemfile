c0756d60af2e976656d6d353d6a7f78e3b48881d
girlesa <girlesau@gmail.com>
2018-10-06 13:28:52 -0500

    Autenticacion, creacion de libros , session

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ ruby '2.4.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
+
+gem 'devise', '~> 4.5'
+
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,15 +1,24 @@
 class BookController < ApplicationController
-    
+    before_action :authenticate_usuario!, only: [:new, :create, :delete]
     def index
         # Hacemos una consulta de todos los libros.
         @books=Book.all
     end
 
     def new
+        @authors =Author.all
+         # variable de clase, la puede usar en toda la clase o en los templates asociados a ella
+        #trae todos los objetos de la tabla autor
+        #el otro trae el primer registro
+       # y el primer objetio que coincida con mi búsqueda
+        #Author.find()
+        #Author.find_by(name)
+        #Author.where()
+      
     end
 
     def create
-        book_params=params.require(:book).permit(:title, :description)
+        book_params=params.require(:book).permit(:title, :description, :author_id)
         Book.create(book_params)
         redirect_to action: 'index'
     end
